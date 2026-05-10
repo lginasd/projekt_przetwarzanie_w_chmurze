79086ad3242594ea57b8b5540b467ae7f7a6b365
lginasd <wa14ls@proton.me>
2026-05-10 15:09:08 +0200

    fix: product documentation

--- app/schemas/product.py
+++ app/schemas/product.py
@@ -4,15 +4,15 @@ from pydantic import BaseModel, Field
 
 
 class ProductCreate(BaseModel):
-    name: str
-    price: Decimal = Field(gt=0)
+    name: str = Field(examples=["Laptop Dell"])
+    price: Decimal = Field(gt=0, examples=[42.99])
     quantity: int = Field(ge=0, default=0)
 
 
 class ProductResponse(BaseModel):
     id: int
-    name: str
-    price: Decimal
+    name: str = Field(examples=["Laptop Dell"])
+    price: Decimal = Field(gt=0, examples=[42.99])
     quantity: int
 
     class Config:
@@ -20,6 +20,6 @@ class ProductResponse(BaseModel):
 
 
 class ProductUpdate(BaseModel):
-    name: str
-    price: Decimal = Field(gt=0)
+    name: str = Field(examples=["Laptop Dell"])
+    price: Decimal = Field(gt=0, examples=[42.99])
     quantity: int = Field(ge=0, default=0)
