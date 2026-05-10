3171d92c9a4080b4a009657ae80cf46ab63872ae
lginasd <wa14ls@proton.me>
2026-05-10 14:06:19 +0200

    feature: global exception handler

--- app/main.py
+++ app/main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from starlette.responses import JSONResponse
 
 from app.database import engine, DataBase
 from app.routers import auth, order, product, user
@@ -22,4 +23,9 @@ app.include_router(order.router)
 app.include_router(product.router)
 app.include_router(user.router)
 
-# TODO: @app.exception_handler(...)
+@app.exception_handler(Exception)
+def global_exception_handler(request, exc):
+    return JSONResponse(
+        status_code=500,
+        content={"detail": "Internal server error"}
+    )
